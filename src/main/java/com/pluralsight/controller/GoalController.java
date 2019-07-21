7bf0c994f4957162e4523b8337522335be95f8d6
martin.arnesi <martin.arnesi@endava.com>
2019-07-21 19:54:56 -0300

    Archivos Finales de Spring Data JPA

--- src/main/java/com/pluralsight/controller/GoalController.java
+++ src/main/java/com/pluralsight/controller/GoalController.java
@@ -1,7 +1,10 @@
 package com.pluralsight.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,10 +14,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.pluralsight.model.Goal;
+import com.pluralsight.model.GoalReport;
+import com.pluralsight.service.GoalService;
 
 @Controller
 @SessionAttributes("goal")
 public class GoalController {
+	
+	@Autowired
+	private GoalService goalService;
 
 	@RequestMapping(value = "addGoal", method = RequestMethod.GET)
 	public String addGoal(Model model) {
@@ -34,9 +42,27 @@ public class GoalController {
 		
 		if(result.hasErrors()) {
 			return "addGoal";
+		} else {
+			goalService.save(goal);
 		}
 		
 		return "redirect:index.jsp";
 	}
 	
+	@RequestMapping(value = "getGoals", method = RequestMethod.GET)
+	public String getGoals(Model model) {
+		List<Goal> goals = goalService.findAllGoals();
+		
+		model.addAttribute("goals", goals);
+		return "getGoals";
+	}
+	
+	@RequestMapping(value="getGoalReports", method = RequestMethod.GET)
+	public String getGoalReports(Model model) {
+		List<GoalReport> goalReports = goalService.findAllGoalReports();
+		
+		model.addAttribute("goalReports", goalReports);
+		
+		return "getGoalReports";
+	}
 }
